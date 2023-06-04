8bf59af680b5e2393a58cb3b7a31421c5dcef5cc
TimM32 <timmaupin@live.com>
2023-06-04 14:46:17 -0500

    create some changes

--- clients/client.js
+++ clients/client.js
@@ -1,32 +1,32 @@
 // Client module
 
 class Client {
-    constructor(clientId) {
-      this.clientId = clientId;
-      this.server = null;
-    }
+  constructor(clientId) {
+    this.clientId = clientId;
+    this.server = null;
+  }
 
-    connect(server) {
-      this.server = server;
-    }
+  connect(server) {
+    this.server = server;
+  }
 
-    subscribe(targetClientId, eventType) {
-      if (this.server) {
-        this.server.subscribe(this.clientId, targetClientId, eventType);
-      }
-    }
-
-    publish(eventType, message) {
-      if (this.server) {
-        this.server.publish(this.clientId, eventType, message);
-      }
+  subscribe(targetClientId, eventType) {
+    if (this.server) {
+      this.server.subscribe(this.clientId, targetClientId, eventType);
     }
   }
 
-  function createClient(clientId) {
-    return new Client(clientId);
+  publish(eventType, message) {
+    if (this.server) {
+      this.server.publish(this.clientId, eventType, message);
+    }
   }
+}
 
-  module.exports = {
-    createClient,
-  };
+function createClient(clientId) {
+  return new Client(clientId);
+}
+
+module.exports = {
+  createClient,
+};
