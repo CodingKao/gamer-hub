49456de2e304becf6189758f7353934b0fa1794e
Kao Saelor <saelor.kao@gmail.com>
2023-06-04 00:18:33 -0700

    added some files

---
+++ clients/client.js
@@ -0,0 +1,32 @@
+// Client module
+
+class Client {
+    constructor(clientId) {
+      this.clientId = clientId;
+      this.server = null;
+    }
+
+    connect(server) {
+      this.server = server;
+    }
+
+    subscribe(targetClientId, eventType) {
+      if (this.server) {
+        this.server.subscribe(this.clientId, targetClientId, eventType);
+      }
+    }
+
+    publish(eventType, message) {
+      if (this.server) {
+        this.server.publish(this.clientId, eventType, message);
+      }
+    }
+  }
+
+  function createClient(clientId) {
+    return new Client(clientId);
+  }
+
+  module.exports = {
+    createClient,
+  };
--- weather/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { io } = require('socket.io-client');
-const socket = io('http://localhost:3001/brightness');
-
-
-setTimeout(() => {
-  socket.emit('JOIN', 'sun');
-}, 3000);
