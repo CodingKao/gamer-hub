ae1e35dcfc99471ccb899afa5e28baf0f40f35e9
Kao Saelor <saelor.kao@gmail.com>
2023-06-05 10:52:15 -0700

    added disconnect/connect, and user name

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3001;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  // Broadcast a message to all connected users when someone connects
+  io.emit('chat message', 'A user has connected');
+
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', () => {
+    // Broadcast a message to all connected users when someone disconnects
+    io.emit('chat message', 'A user has disconnected');
+  });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log('listening on:', PORT);
 });
